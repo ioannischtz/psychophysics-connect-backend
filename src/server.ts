dbfc0dc74bbe96d75790953ad2b4e5e1d7489b21
ioannis chatzianagnostou <ioannischtz@gmail.com>
2023-08-19 16:45:57 +0300

    feat: Impement the homepage-routes handlers.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { CookieOptions } from "express";
 import initDB from "./db/initDB.js";
 import userRoutes from "./routes/users.js";
-import { port } from "./config.js";
+import homepageRoutes from "./routes/homepage.js";
+import { environment, jwtSecret, port } from "./config.js";
 import bodyParser from "./middleware/bodyParser.js";
 import { Middleware } from "middleware.js";
+import createSession from "./middleware/session.js";
 
 const connection = await initDB();
 
@@ -16,7 +18,24 @@ const app = express();
 //   encoding: "utf-8",
 // });
 
+const defaultSessionConfig = {
+  key: "user.sess",
+  cookieOpts: {
+    httpOnly: true,
+    maxAge: 86400000,
+    secure: environment === "production",
+    signed: true, // Important: enable signed cookies,
+    sameSite: "strict",
+  } as CookieOptions,
+  secret: jwtSecret, // Use the jwtSecret from ../config.js
+};
+
+// Enable the cookie/browser session middleware
+app.use(createSession(defaultSessionConfig));
+
+// Enable the api routes
 app.use("/api/users", userRoutes);
+app.use("/api/homepage", homepageRoutes);
 
 app.get("/", (req, res) => res.send("Server up!"));
 
